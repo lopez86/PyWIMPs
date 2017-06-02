768bb523ca5419ca2a0b717198a44774fa1499f6
Jeremy Lopez <jeremy.lopez@colorado.edu>
2017-06-02 00:28:43 -0600

    A class to calculate the WIMP speed and direction in the lab frame. Also a namespace to hold coordinates of various places to use as inputs for the coordinate transform

---
+++ wimpsim/astro/Coordinates.py
@@ -0,0 +1,98 @@
+import numpy as np
+import astropy.time as astime
+import astropy.coordinates as ascoords
+from astropy import units as u
+from .. import units
+
+class Coordinates:
+    """ 
+    Class to calculate things like the direction and mean velocity of WIMPs
+    in the dark matter halo.
+
+    Equations for motion follow:
+    Lewin, J.D. and Smith, P.F., Astropart. Phys. 6 (1996) 87-112.
+    This is still referenced in more recent sources such as:
+    Green, A.M. Phys. Rev. D68 (2003) 023004.
+    and
+    Freese, Lisanti, and Savage. Rev. Mod. Phys. 85 (2013) 1561.
+
+
+    """ 
+    def __init__(self):
+        self.ur = np.array([0,230,0]) * units.km /units.sec
+        self.us = np.array([9,12,7]) * units.km / units.sec
+        self.beta = np.array([-5.503,59.575,29.812]) * units.deg
+        self.lambda_ = np.array([266.141,-13.3485,179.3212]) * units.deg
+        self.uEmean = 29.79 * units.km / units.sec
+        self.ellipticity = 0.016722
+        self.lambda0 = 13 * units.deg
+        self.lambdaL0 = 280.460 * units.deg
+        self.lambdaL1 = 0.9856474 * units.deg
+        self.lambdag0 = 357.528 * units.deg
+        self.lambdag1 = 0.9856003 * units.deg
+        self.lambdaB = 1.915 * units.deg
+        self.lambdaC = 0.020 * units.deg
+        self.J2000 = dt.datetime(1999,12,31,12,0,0,tzinfo = dt.timezone.utc)
+
+        self.earthSpeed = 6371. * units.km / (86400. * units.sec)
+
+    def earth_motion_gal(self,timestamp):
+        """ 
+        Get the motion of Earth in galactic coordinates 
+        A datetime object is needed in order to get the position
+        around the sun
+        """
+        timedelta = dt.datetime.fromtimestamp(timestamp) - self.J2000
+        days = timedelta.total_seconds() / 86400.
+        L = self.lambdaL0 + self.lambdaL1 * days
+        g = self.lambdag0 + self.lambdag1 * days
+        lambda_t = L + self.lambdaB * np.sin(g) + self.lambdaC * np.sin(2*g)
+        uE_l = uEmean * (1 - self.ellipticity * np.sin(lambda_t - self.lambda0 ) )
+        uE = uEl * np.cos(self.beta) * np.sin(lambda_t - self.lambda_)
+        return self.ur + self.us + uE
+
+    def earth_motion_lab(self,timestamp,coord):
+        """
+        Get the velocity of the lab frame in local coordinates. 
+        The coordinate system here is:
+        (r,theta,phi) = (1,0,x) = up
+        (r,theta,phi) = (1,90 deg, 0) = east
+        (r,theta,phi) = (1,90 deg, 90 deg) = north
+
+        This uses AstroPy to convert the galactic frame velocity
+        into the altitude/azimuth system for a given time and 
+        position. It then adds a small correction from the 
+        rotation of Earth. There will be a very slight 
+        sidereal modulation of the velocity.
+        """
+        lat,lon = coord
+        loc = ascoords.EarthLocation(lon=lon*u.deg,lat=lat*u.deg)
+        time = astime.Time(timestamp) # A datetime
+        earth_gal = self.earth_motion_gal(timestamp)
+        earth_vel = np.sqrt(earth_gal.dot(earth_gal))
+        gal_lon = np.arctan2(earth_gal[2],earth_gal[1])
+        gal_lat = 0.5 * np.pi - np.arccos(earth_gal[3] / earth_vel)
+
+
+        gal = ascoords.Galactic(l = gal_lon * u.rad,b = gal_lat * u.rad)
+        gal_0 = SkyCoord(gal)
+        altaz = ascoords.altAz(obstime=time,location=loc,pressure=0)
+        altaz_coords = gal_0.transform_to(altaz)
+        
+        # Let's let x = east, y = north, z = up
+
+        az = altaz_coords.data.lon.to(np.rad).value
+        alt = altaz_coords.data.lat.to(np.rad).value
+        v_earth = np.zeros(3)
+        theta = 0.5 * np.pi - alt
+        phi = 0.5 * np.pi - az
+        v_earth[0] = np.sin(theta) * np.cos(phi) * earth_vel
+        v_earth[1] = np.sin(theta) * np.sin(phi) * earth_vel
+        v_earth[2] = np.cos(theta) * earth_vel
+
+
+        # The lab frame is also traveling west due to Earth's rotation
+        # Earth's rotation ends up being:
+        v_rot = np.array([-self.earthSpeed,0,0])
+        v_tot = v_earth + v_rot
+        return v_tot
---
+++ wimpsim/astro/locations.py
@@ -0,0 +1,59 @@
+""" This file contains locations of various places around the world """
+import numpy as np
+
+# Cities
+
+# New York City
+NewYork = np.array([40 + 42./60 +46./3600, -74 - 21./3600])
+# Cambridge, MA
+Cambridge = np.array([42+22./60 + 25./3600,-71 - 6./60 - 38./3600])
+
+# National/International Labs
+
+# Batavia, IL (Chicago)
+Fermilab = np.array([41+49./60+55./3600,-88-15./60-26./3600])
+# Meyrin, CH (Geneva)
+SLAC = np.array([37+25./60+12.7/3600,-122-12./60-16.46/3600])
+# Upton, NY (Long Island)
+Brookhaven = np.array([40.873346,-72.872057])
+# Los Alamos, NM
+LosAlamos = np.array([35+52./60+32./3600,-106-19./60-27./3600])
+# Oak Ridge, TN
+OakRidge = np.array([35.93,-84.31])
+# Tsukuba, Ibaraki, JP
+CERN = np.array([46+14./60+3./3600,6+3./60+10./3600])
+# Palo Alto, CA
+KEK = np.array([36+8./60+55./3600,140+4./60+37./3600])
+# Dubna, RU (Moscow)
+JINR = np.array([56+44./60+47./3600,37+11./60+21./3600])
+# Vancouver, BC
+TRIUMF = np.array([49 + 14./60+52./3600,-123-13./60-50./3600])
+# Hamburg, DE
+DESY = np.array([53+34./60+33./3600,9+52./60+46./3600])
+
+# Underground Facilities
+
+# SNOLAB in Sudbury, ON
+SNOLAB = np.array([46+28.3/60,-81-11.2/60])
+# Sanford Underground Research Facility
+# Homestake Mine, Lead, SD
+SURF = np.array([44.352,-103.751])
+# Kamioka Observatory
+# Mozumi, Hida, Gifu, JP (near Toyama)
+Kamioka = np.array([36+25.6/60,137+18.7/60])
+# Waste Isolation Pilot Plant
+# Carlsbad, NM
+WIPP = np.array([32+22./60+18./3600,-103-47./60-37./3600])
+# Boulby Mine
+# Durham, Eng., UK
+Boulby = np.array([54.5534,-0.8245])
+# Gran Sasso Lab (LNGS)
+# Gran Sasso, Italy
+GranSasso = np.array([42.454,13.576])
+# Canfranc Underground Laboratory
+# Canfranc, ES (Pyrenees)
+Canfranc = np.array([42.749174,-0.516586]) # From Google Maps
+# Rural Sichuan
+CJPL = np.array([28.15323,101.7114])
+
+
