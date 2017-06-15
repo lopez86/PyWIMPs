f8d6548c6c19e973222af25553d9c665d0acb140
Jeremy Lopez <lopez86@users.noreply.github.com>
2017-06-14 18:35:51 -0600

    Added documentation for the last couple form factors used right now.

--- wimp/xsec/EngelFormFactor.py
+++ wimp/xsec/EngelFormFactor.py
@@ -1,15 +1,51 @@
+""" EngelFormFactor.py
+
+A parameterized spin-dependent form factor mentioned by
+Lewin and Smith and attributed to Engel.
+
+"""
+__author__ = 'Jeremy P. Lopez'
+__date__ = 'June, 2017'
+__copyright__ = '(c) 2017, Jeremy P. Lopez'
 from . import FormFactor
 from .. import units
 
+
 class EngelFormFactor(FormFactor):
+    """
+    This returns a spin-dependent form factor.
+
+    The definitions of the parameters are found
+    in Lewin and Smith. This is attributed to Engel.
+
+    This form factor is a sinc function with the
+    first zero suppressed.
+
+    Attributes:
+        A: The mass number
+        a: Used in radius calculation
+        b: Used in radius calculation
+     
+    """
+
 
     def __init__(self):
         self.A = 1
-        self.calculate_params()
         self.a = 1.0 * units.fm
         self.b = 0
+        self.calculate_params()
 
     def set_params(self,pars):
+        """ Set parameters from a dictionary.
+
+            Args:
+                pars: {string}
+
+            Parameters:
+                AtomicNumber: A
+                EngelFFRnA: a
+                EngelFFRnB: b
+        """
         if 'AtomicNumber' in pars.keys():
             self.A = pars['AtomicNumber']
         if 'EngelFFRnA' in pars.keys():
@@ -18,11 +54,21 @@ class EngelFormFactor(FormFactor):
             self.b = pars['EngelFFRnB']
         self.calculate_params()
 
-    def calculate_params(self):
+    def initialize(self):
+        """ Same as calculate_params()."""
+        self.calculate_params()
 
+    def calculate_params(self):
+        """ Calculate some useful parameters."""
         self.rn = (self.a * self.A ** (1./3) + self.b) 
 
     def ff2(self,Q2):
+        """ Calculate |F(Q^2)|^2.
+ 
+            Args:
+                Q2: Recoil squared momentum transfer
+        """
+
         qrn = np.sqrt(Q2) * rn / units.hbarc
         if 2.55 < qrn < 4.5:
           return 0.047
--- wimp/xsec/HelmFormFactor.py
+++ wimp/xsec/HelmFormFactor.py
@@ -1,9 +1,39 @@
+""" HelmFormFactor.py
+
+The form factor proposed by Helm discussed in the 
+Lewin & Smith dark matter review.
+
+"""
+__author__ = 'Jeremy P. Lopez'
+__date__ = 'June, 2017'
+__copyright__ = '(c) 2017, Jeremy P. Lopez'
+
 from . import FormFactor
 from .. import units
 
 class HelmFormFactor(FormFactor):
+    """
+    This returns the Helm form factor.
+
+    The definitions of the parameters are found
+    in Lewin and Smith.
+
+    The Helm form factor has the form
+    |F(Q^2)|^2 = 9 |j1(Qrn) / Qrn |^2 exp(-(Qs)^2)
+
+    Attributes:
+        A: The mass number
+        ca: Used in radius calculation
+        cb: Used in radius calculation
+        a: Used in radius calculation
+        s: Used in radius calculation
+     
+    """
 
     def __init__(self):
+        """ Initialize to hydrogen and the default
+            numbers proposed by Lewin and Smith.
+        """
         self.A = 1
         self.ca = 1.23 * units.fm
         self.cb = 0.6 * units.fm
@@ -13,6 +43,19 @@ class HelmFormFactor(FormFactor):
 
 
     def set_params(self,pars):
+        """ Set parameters from a dictionary.
+
+            Args:
+                pars: {string}
+
+            Parameters:
+                AtomicNumber: A
+                HelmCa: ca
+                HelmCb: cb
+                HelmA: a
+                HelmS: s
+ 
+        """
         if 'AtomicNumber' in pars.keys():
             self.A = pars['AtomicNumber']
         if 'HelmCa' in pars.keys():
@@ -26,9 +69,12 @@ class HelmFormFactor(FormFactor):
 
         self.calculate_params()
     
+    def initialize(self):
+        """ Same as calculate_params()"""
+        self.calculate_params()
 
     def calculate_params(self):
-
+        """ Calculate some useful parameters. """
         self.c = (self.ca * self.A ** (1./3)  - self.cb) 
         self.rn = np.sqrt(self.c*self.c + 7./3 * \
                   np.pi * np.pi * self.a * self.a - \
@@ -37,6 +83,11 @@ class HelmFormFactor(FormFactor):
 
 
     def ff2(self,Q2):
+        """ Calculate |F(Q^2)|^2.
+ 
+            Args:
+                Q2: Recoil squared momentum transfer
+        """
         qrn = np.sqrt(Q2)*self.rn / units.hbarc
          
         j1 = np.sin(qrn) / (qrn*qrn) - np.cos(qrn) / qrn
